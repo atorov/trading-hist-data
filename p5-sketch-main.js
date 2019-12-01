11cb1898edf67c7bd92c9a0e7ca4696615033606
atorov <veselin.lkp@gmail.com>
2019-12-01 13:33:02 +0200

    implementing sma 200, 50, 21 and 8

--- p5-sketch-main.js
+++ p5-sketch-main.js
@@ -1,7 +1,7 @@
 const p5Main = new p5((sketch) => {
     const s = sketch
 
-    const FRAME_RATE = 0.60
+    const FRAME_RATE = 60
 
     const POINTER_OFFSET = 200 * 2
 
@@ -39,6 +39,7 @@ const p5Main = new p5((sketch) => {
         // ---------------------------------------------------------------------
         s.background(255)
 
+        s.strokeWeight(1)
         s.stroke(0)
         s.noFill()
         s.rect(0, 0, s.width, s.height)
@@ -70,6 +71,7 @@ const p5Main = new p5((sketch) => {
 
                 const color = open > close ? [255, 0, 0] : [0, 255, 0]
 
+                s.strokeWeight(1)
                 s.stroke(127)
                 s.line(
                     i * (s.width / histDataCandlesChunk.length) + (s.width / histDataCandlesChunk.length / 2),
@@ -88,10 +90,94 @@ const p5Main = new p5((sketch) => {
             }
 
             // SMA 200 ---------------------------------------------------------
-            // const sma200 = histDataChunk
-            // .slice()
-            // .reduce((acc, row) => acc + row[3], 0) / POINTER_OFFSET
-            // console.log(sma200)
+            const SMA200_WINDOW = 200
+            let histDataSMA200 = []
+            for (let i = 0; i < HIST_DATA_CANDLES_CHUNK_WINDOW; i++) {
+                const chunk = i ? histDataChunk.slice(-(i + SMA200_WINDOW), -i) : histDataChunk.slice(-(i + SMA200_WINDOW))
+                const sma200 = chunk.reduce((acc, row) => acc + row[3], 0) / SMA200_WINDOW
+                histDataSMA200 = [sma200, ...histDataSMA200]
+            }
+            // console.log(histDataSMA200)
+
+            for (let i = 0; i < histDataSMA200.length; i++) {
+                s.strokeWeight(13)
+                s.stroke(0, 0, 127, 63)
+                s.noFill()
+                s.line(
+                    i * (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW) + (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW / 2),
+                    (1 - histDataSMA200[i]) * s.height,
+                    (i - 1) * (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW) + (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW / 2),
+                    (1 - histDataSMA200[i - 1]) * s.height,
+                )
+            }
+
+            // SMA 50 ----------------------------------------------------------
+            const SMA50_WINDOW = 50
+            let histDataSMA50 = []
+            for (let i = 0; i < HIST_DATA_CANDLES_CHUNK_WINDOW; i++) {
+                const chunk = i ? histDataChunk.slice(-(i + SMA50_WINDOW), -i) : histDataChunk.slice(-(i + SMA50_WINDOW))
+                const sma200 = chunk.reduce((acc, row) => acc + row[3], 0) / SMA50_WINDOW
+                histDataSMA50 = [sma200, ...histDataSMA50]
+            }
+            // console.log(histDataSMA50)
+
+            for (let i = 0; i < histDataSMA50.length; i++) {
+                s.strokeWeight(8)
+                s.stroke(0, 0, 255, 63)
+                s.noFill()
+                s.line(
+                    i * (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW) + (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW / 2),
+                    (1 - histDataSMA50[i]) * s.height,
+                    (i - 1) * (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW) + (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW / 2),
+                    (1 - histDataSMA50[i - 1]) * s.height,
+                )
+            }
+
+            // SMA 21 ----------------------------------------------------------
+            const SMA21_WINDOW = 21
+            let histDataSMA21 = []
+            for (let i = 0; i < HIST_DATA_CANDLES_CHUNK_WINDOW; i++) {
+                const chunk = i ? histDataChunk.slice(-(i + SMA21_WINDOW), -i) : histDataChunk.slice(-(i + SMA21_WINDOW))
+                const sma21 = chunk.reduce((acc, row) => acc + row[3], 0) / SMA21_WINDOW
+                histDataSMA21 = [sma21, ...histDataSMA21]
+            }
+            // console.log(histDataSMA21)
+
+            for (let i = 0; i < histDataSMA21.length; i++) {
+                s.strokeWeight(5)
+                s.stroke(0, 127, 255, 63)
+                s.noFill()
+                s.line(
+                    i * (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW) + (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW / 2),
+                    (1 - histDataSMA21[i]) * s.height,
+                    (i - 1) * (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW) + (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW / 2),
+                    (1 - histDataSMA21[i - 1]) * s.height,
+                )
+            }
+
+            // SMA 8 ----------------------------------------------------------
+            const SMA8_WINDOW = 8
+            let histDataSMA8 = []
+            for (let i = 0; i < HIST_DATA_CANDLES_CHUNK_WINDOW; i++) {
+                const chunk = i ? histDataChunk.slice(-(i + SMA8_WINDOW), -i) : histDataChunk.slice(-(i + SMA8_WINDOW))
+                const sma8 = chunk.reduce((acc, row) => acc + row[3], 0) / SMA8_WINDOW
+                histDataSMA8 = [sma8, ...histDataSMA8]
+            }
+            // console.log(histDataSMA8)
+
+            for (let i = 0; i < histDataSMA8.length; i++) {
+                s.strokeWeight(3)
+                s.stroke(0, 255, 255, 63)
+                s.noFill()
+                s.line(
+                    i * (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW) + (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW / 2),
+                    (1 - histDataSMA8[i]) * s.height,
+                    (i - 1) * (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW) + (s.width / HIST_DATA_CANDLES_CHUNK_WINDOW / 2),
+                    (1 - histDataSMA8[i - 1]) * s.height,
+                )
+            }
+
+            // s.noLoop()
         }
         else {
             s.noLoop()
